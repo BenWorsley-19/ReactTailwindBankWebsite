927b2aa0eada36820657bc7bb49d1b855c379576
ben <ben.worsley@matillion.com>
2023-11-12 21:15:27 +0000

    feat: nav, hero, billing sections

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Stats from "./Stats"
+import Billing from "./Billing";
+
+
+export {
+    Navbar,
+    Hero,
+    Billing,
+    Stats
+};
